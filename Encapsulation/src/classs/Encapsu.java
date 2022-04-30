d7e792d04780ed1daad19287cc93a1171bcaee58
patid <patid@DESKTOP-HOBCA4B>
2022-04-30 17:33:21 +0530

    Add

---
+++ Encapsulation/src/classs/Encapsu.java
@@ -0,0 +1,14 @@
+package classs;
+
+public class Encapsu {
+	
+	
+	public static int c = 555;
+	public static int d = 996;
+	private static int a =90;
+	private int b;
+	public static void main(String[] args) {
+		System.out.println(a+c);
+	}
+
+}
---
+++ Encapsulation/src/classs/Encapsu1.java
@@ -0,0 +1,13 @@
+package classs;
+
+public class Encapsu1   {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(Encapsu.c);
+		System.out.println(Encapsu.a);
+		System.out.println(Encapsu.c+Encapsu.d);
+		
+	}
+
+}
